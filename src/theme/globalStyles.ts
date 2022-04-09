6bcf00ec570c26b288332cd95c2a05c97a53d698
kuttim <kuttim.mikk@gmail.com>
2022-04-09 17:56:21 +0300

    background instead of color

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     body {
-        color: #000000;
+        background: #0f0f22;
         font-family: 'Roboto', sans-serif;
     }
 `;
