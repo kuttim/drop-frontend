4ac9a60ec81c5f07fb7e56f0a65afc8deb6e04a4
kuttim <kuttim.mikk@gmail.com>
2022-04-09 23:11:35 +0300

    hero content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+
 import './theme/global.scss';
 function App() {
   return (
     <Fragment>
       <Header />
+      <Hero />
     </Fragment>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+function Hero() {
+  return (
+    <Fragment>
+      <div className="hero-content">
+        <span className="logo-text">Drop</span>
+      </div>
+      <div className="hero-text">
+        <p>
+          Most link shorteners do too much. <br></br>This one just
+          makes your links shorter.
+        </p>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Hero;
